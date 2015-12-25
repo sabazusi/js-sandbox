bffd74968b0d099a24dcc21c403749c4387f98c7
sabazusi <kouhei.live@gmail.com>
2015-12-25 16:06:12 +0900

    add text-adder.

---
+++ react-flexbox/index.js
@@ -0,0 +1,5 @@
+onClickButton = () => {
+	var inner = document.getElementById("target").innerHTML;
+	var newinner = "bbbbbbb<br>" + inner;
+	document.getElementById("target").innerHTML = newinner;
+};
