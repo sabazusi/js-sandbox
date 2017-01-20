5fe09e1f59efdcb6aa0ebbe5eb2108361b799088
sabazusi <kohei_tsurubo@dwango.co.jp>
2017-01-20 20:27:43 +0900

    Add basic tutorial files

--- grimoire/src/index.js
+++ grimoire/src/index.js
@@ -1,1 +1,9 @@
-alert('hello');
+import gr from "grimoirejs";
+
+gr(() => {
+  const main = gr("#main");
+  setTimeout(() => {
+  alert(1);
+    main.setAttribute("position", "3,0,0");
+  }, 1000);
+});
