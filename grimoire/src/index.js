904a740b527a0b80130e30a9e1278c593636caec
sabazusi <kouhei.live@gmail.com>
2017-01-21 16:59:07 +0900

    Fix bug

--- grimoire/src/index.js
+++ grimoire/src/index.js
@@ -1,9 +1,6 @@
-import gr from "grimoirejs";
-
 gr(() => {
   const main = gr("#main");
   setTimeout(() => {
-  alert(1);
-    main.setAttribute("position", "3,0,0");
+    main("#green-mesh").setAttribute("position", "3,0,0");
   }, 1000);
 });
