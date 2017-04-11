4a59cf44f91f4ab20735fdcdddc608a5f2522310
sabazusi <kouhei.live@gmail.com>
2017-04-12 00:16:18 +0900

    Add lodash.debounce and debounced input changing

--- storybook-sandbox/src/components/DebounceInput/DebounceInput.js
+++ storybook-sandbox/src/components/DebounceInput/DebounceInput.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import debounce from 'lodash.debounce';
 
 class DebounceInputComponent extends React.Component {
-  onChangeInput(e) {
+  componentWillMount() {
+    this.onChangeInput = debounce(this.props.onChange, 200);
+  }
+
+  onChangeInputForDebounce(e) {
     this.props.onChange(e.target.value);
-  };
+  }
 
   render() {
     return (
       <div>
         <input
           style={{width: 800}}
-          onChange={this.onChangeInput.bind(this)}
+          onChange={(e) => this.onChangeInput(e.target.value)}
         />
       </div>
     )
@@ -21,18 +26,25 @@ export default class DebounceInput extends React.Component {
   constructor() {
     super();
     this.state = {
-      value: ""
+      value: "",
+      updateCount: 0
     }
   }
   render() {
     return (
       <div>
         <DebounceInputComponent
-          onChange={(value) => this.setState({value})}
+          onChange={(value) => this.setState({
+            value,
+            updateCount: this.state.updateCount+1
+          })}
         />
 
         <div>
           <span>onChange: {this.state.value}</span>
+        </div>
+        <div>
+          <span>updateCount: {this.state.updateCount}</span>
         </div>
       </div>
     );
